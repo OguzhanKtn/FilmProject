925599a26625fc5b10118df6fa4aea0d1c5c73cf
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2023-04-23 00:46:36 +0300

    SearchView-Added

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,27 @@
 // https://api.themoviedb.org/3/search/movie?api_key=289bb04bf03ab954dd0eea031def1053&language=en-US&page=1&include_adult=false
 
 import Search from "./models/search";
+import { elements } from "./base";
+import * as searchView from './views/searchView';
 
-const search = new Search("abc");
-console.log(search);
+const state = {};
 
-search.getResult();
+const searchController = async() =>{
+    const keyword = elements.searchInput.value;
+
+    if(keyword){
+       state.search = new Search(keyword);
+
+      await state.search.getResult();
+
+      searchView.displayResults(state.search.data);
+    }else{
+        alert("you must enter a keyword");
+    }
+}
+
+elements.searchForm.addEventListener("submit",function(e){
+    e.preventDefault();
+    searchController();
+
+})
